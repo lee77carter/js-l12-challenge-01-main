eba5c3adca460bc0d3352a71583bac32245ec651
lee_c <gitleelee@gmail.com>
2023-11-18 23:18:30 -0800

    Added functions to display images and an event listener.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,29 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
-
+// API setup & random image selection
 const getImage = async function () {
     const res = await fetch("https://picsum.photos/v2/list?limit=100");
-
     const images = await res.json();
-    console.log(images);
+    selectRandomImage(images);
+};
+// Display random images
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    //console.log(randomIndex);
+    const randomImage = images[randomIndex];
+    //console.log(randomImage);
+    displayImage(randomImage);
 };
 
-getImage();
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+// Click Event to get images
+button.addEventListener("click", function () {
+    getImage();
+});
